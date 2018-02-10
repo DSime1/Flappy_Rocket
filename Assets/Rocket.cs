1c589697402f2d4612731fc7f450e9f2d0e13a88
‘Davide <Simeone.davi@gmail.com>
2018-02-10 13:28:04 +0100

    Initial commit

---
+++ Assets/Rocket.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource thruster;
+
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        thruster = GetComponent<AudioSource>();
+
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ProcessInput();
+
+
+	}
+
+    private void ProcessInput()
+    {
+            if (Input.GetKey(KeyCode.Space)){
+            
+            if (thruster.isPlaying){
+
+                print("Playing Rocket");
+
+            } else {thruster.Play();}
+
+            rigidBody.AddRelativeForce(0,(900 * Time.deltaTime),0);
+
+
+            print("Space Pressed");
+
+
+        }else {
+
+
+            thruster.Stop(); } 
+
+
+        if (Input.GetKey(KeyCode.A)){
+
+            print("rotate Left");
+
+            transform.Rotate(0,0,(100*Time. deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0,(-100 * Time.deltaTime));
+        }
+
+
+
+    }
+}
