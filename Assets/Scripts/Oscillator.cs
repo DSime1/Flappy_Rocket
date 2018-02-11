3fecd7e7b594f7cf7fb7c5beebcc61f1a6ae73b9
‘Davide <Simeone.davi@gmail.com>
2018-02-11 16:27:33 +0100

    obstacle movements
    Added spot lights + obstacle movements scripts

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Prevents to add 2 scripts to same object
+[DisallowMultipleComponent]
+
+public class Oscillator : MonoBehaviour {
+
+    GameObject redObstacles;
+
+    [SerializeField] float period = 2f;
+
+    [SerializeField] Vector3 movementVector;
+
+    [Range(0, 1)] float movementFactor;
+
+    Vector3 startingPos;
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+        movementVector = new Vector3 (0, 4, 0);
+
+        redObstacles = GameObject.Find("MovingObstacles");
+
+        startingPos = redObstacles.transform.position ;
+
+       // print(redObstacles.transform.position);
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+
+        //prevent NaN when inserting period = zero.
+        if (period <= Mathf.Epsilon)
+        {
+            print("enter valid Period");
+            return;
+        }
+
+
+        float cycles = Time.time / period;
+
+        const float tau =  Mathf.PI * 2f;
+
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave/2f + 0.5f;
+
+        offset = movementVector * movementFactor;
+
+        //movementVector = startingPos;
+        redObstacles.transform.position = startingPos + offset;
+
+        //redObstacles.transform.position = movementVector * movementFactor;
+        print(movementVector);
+        print(redObstacles.transform.position);
+	}
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource thruster;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThruster = 1000f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip Explosion;
+    [SerializeField] AudioClip Win;
+    [SerializeField] float loadLevelDelay = 2f;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem ExplosionParticles;
+    [SerializeField] ParticleSystem WinParticles;
+
+
+
+    float rotationFrame;
+    float thrustFrame;
+
+    enum State { Alive, Dying, Transcending };
+    State state;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        thruster = GetComponent<AudioSource>();
+        rotationFrame = rcsThrust * Time.deltaTime;
+        thrustFrame = mainThruster * Time.deltaTime;
+
+         state = State.Alive;
+
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.Alive)
+        {
+            RespondToRotation();
+            RespondToThrustInput();
+        }
+
+
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            //print("Collision detected"); 
+
+            if (state != State.Alive){
+
+                return;
+            }
+
+            switch (collision.gameObject.tag){
+
+                case "Friendly":
+                    break;
+
+                case "Finish":
+
+                    StartSuccessSequence();
+
+                    break;
+
+                default:
+
+                    StartDeathSequence();
+
+                    break;
+            }
+        }
+
+
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        thruster.Stop();
+        mainEngineParticles.Stop();
+        thruster.PlayOneShot(Explosion);
+        ExplosionParticles.Play();
+
+        Invoke("LoadFirstLevel", loadLevelDelay);
+    }
+
+    private void StartSuccessSequence()
+    {
+        print("hit Finish");
+        thruster.Stop();
+        mainEngineParticles.Stop();
+        thruster.PlayOneShot(Win);
+        WinParticles.Play();
+
+        state = State.Transcending;
+
+        Invoke("LoadNextLevel", loadLevelDelay); //Invoke requires method name as a string, 1f means 1 second delay
+    }
+
+    private void LoadFirstLevel()
+    {
+        
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        
+        SceneManager.LoadScene(1);
+    }
+
+    private void RespondToRotation()
+    {
+     
+        rigidBody.freezeRotation = true; //Manually set rotation
+        if (Input.GetKey(KeyCode.A))
+        {
+            //print("rotate Left");
+
+            transform.Rotate(Vector3.forward * rotationFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationFrame);
+        }
+        rigidBody.freezeRotation = false; //reset natural rotation
+            
+
+      
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            
+            ApplyThrust();
+
+        }
+        else
+        {
+
+            thruster.Stop();
+            mainEngineParticles.Stop();
+        }
+
+
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * thrustFrame);
+        if (!thruster.isPlaying)
+        {
+            thruster.PlayOneShot(mainEngine);
+            mainEngineParticles.Play();
+
+        }
+
+    }
+}
